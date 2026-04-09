a3277c0b542e23a5f80a7f394e8d6db2839c53a1
evanbern59 <evansantos1202@gmail.com>
2026-04-08 22:06:52 -0700

    added event controller

---
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -0,0 +1,44 @@
+﻿using COMP003B.Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class EventController
+    {
+        [HttpGet("event/register/{eventCode}")]
+        public IActionResult Register(string eventCode)
+        {
+            EventRegistration registration = new EventRegistration();
+            {
+                EventCode = eventCode
+            };
+
+            return View(registration);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new EventRegistration());
+        }
+
+        [HttpPost]
+        public IActionResult Register(EventRegistration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success",  registration);
+            }
+
+            return View(registration);
+
+        }
+
+        [HttpGet]
+        public IActionResult Success(EventRegistration registration)
+        {
+            return View(registration);
+        }
+    }
+}
