2d156d89cfb8cb4bf988b784182636bc83d1dc78
evanbern59 <evansantos1202@gmail.com>
2026-04-08 22:35:44 -0700

    added event views

--- COMP003B.Assignment3/Controllers/EventController.cs
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace COMP003B.Assignment3.Controllers
 {
-    public class EventController
+    public class EventController : Controller
     {
         [HttpGet("event/register/{eventCode}")]
         public IActionResult Register(string eventCode)
